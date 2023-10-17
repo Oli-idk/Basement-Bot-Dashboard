41cfaa8b369ee4e51911980ba6dfc1f63afc2ff3
AkiraDev <akira.devjs@gmail.com>
2023-10-17 16:05:26 -0700

    fixes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { component$, Slot, useStore } from '@builder.io/qwik';
 import { Menu } from 'qwik-ionicons';
 import { Button } from './elements/Button';
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(({ guild }: any) => {
   const menuStore = useStore({
@@ -10,7 +11,7 @@ export default component$(({ guild }: any) => {
     <aside class="w-full mt-28 align-middle sm:sticky sm:h-1 sm:top-28" aria-label="Sidebar">
       <div class="py-4 px-3 rounded-xl bg-gray-800 border border-gray-700 text-base font-bold mb-4 text-gray-200">
         <div class="flex items-center mb-4">
-          {guild.icon && <img class="w-10 h-10 rounded-full" src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`} alt={guild.name} style={{ 'view-transition-name': 'picture' }} />}
+          {guild.icon && <img class="rounded-full" src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`} alt={guild.name} style={{ 'view-transition-name': 'picture' }} width={10} height={10} />}
           <p class="flex-1 ml-3 text-lg">{guild.name}</p>
         </div>
         <div class="flex">
@@ -40,9 +41,9 @@ export const MenuCategory = component$(({ name }: any) => {
 
 export const MenuItem = component$(({ href, guild }: any) => {
   return (
-    <a href={ guild ? `/dashboard/${guild.id}/${href}` : href} class="min-w-fit transition hover:bg-gray-700 hover:text-white hover:drop-shadow-2xl border border-transparent hover:border-gray-600 px-2 py-1 rounded-lg text-md flex items-center gap-2">
+    <Link href={ guild ? `/dashboard/${guild.id}/${href}` : href} class="min-w-fit transition hover:bg-gray-700 hover:text-white hover:drop-shadow-2xl border border-transparent hover:border-gray-600 px-2 py-1 rounded-lg text-md flex items-center gap-2">
       <Slot />
-    </a>
+    </Link>
   );
 });
 
